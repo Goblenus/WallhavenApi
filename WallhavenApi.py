a860a601b02ecf5fab66233ba8eaabd029b37f60
Goblenus <goblenus@ro.ru>
2016-07-02 20:36:09 +0300

    (Add) Initial commit Features: * Get images urls

---
+++ WallhavenApi.py
@@ -0,0 +1,65 @@
+import logging
+import requests
+from bs4 import BeautifulSoup
+
+
+class WallhavenApi:
+
+    def get_images_urls(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
+                        purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
+                        page=1):
+        image_numbers = \
+            self._get_image_numbers(
+                str(int(category_general)) + str(int(category_anime)) + str(int(category_people)),
+                str(int(purity_sfw)) + str(int(purity_sketchy)) + str(int(purity_nsfw)),
+                resolutions, ratios, sorting, order, page)
+
+        if image_numbers is None:
+            return None
+
+        return self._make_images_urls(image_numbers)
+
+    @staticmethod
+    def _get_image_numbers(categories, purity, resolutions, ratios, sorting, order, page):
+        logging.debug("Trying obtain image number")
+
+        random_page_data = requests.get('https://alpha.wallhaven.cc/search', params={"categories": categories,
+                                                                                     "purity": purity,
+                                                                                     "resolutions": resolutions,
+                                                                                     "ratios": ratios,
+                                                                                     "sorting": sorting,
+                                                                                     "order": order,
+                                                                                     "page": page},
+                                        verify=False)
+
+        logging.debug("Random page request code %d", random_page_data.status_code)
+
+        if random_page_data.status_code != 200:
+            return None
+
+        logging.debug("Random page loaded")
+
+        figures_tags = BeautifulSoup(random_page_data.text, "html.parser")\
+            .select("#thumbs > section:nth-of-type(1) > ul > li > figure[data-wallpaper-id]")
+
+        logging.debug("Found %d image tags", len(figures_tags))
+
+        if not len(figures_tags):
+            return None
+
+        images_numbers = []
+
+        for figure_tag in figures_tags:
+            images_numbers.append(figure_tag.attrs['data-wallpaper-id'])
+
+        return images_numbers
+
+    @staticmethod
+    def _make_images_urls(images_numbers):
+        images_urls = []
+
+        for image_number in images_numbers:
+            images_urls.append('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg'
+                               .format(str(image_number)))
+
+        return images_urls

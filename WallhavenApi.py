4594426c9cdd7204256128c573bcb9d27d732f62
Goblenus <goblenus@ro.ru>
2016-07-08 22:32:00 +0300

    (Ref/Add) Added wallhaven login, code refactoring

--- WallhavenApi.py
+++ WallhavenApi.py
@@ -1,9 +1,57 @@
 import logging
 import requests
 from bs4 import BeautifulSoup
+import threading
 
 
 class WallhavenApi:
+    def __init__(self, username=None, password=None, verify_connection=False):
+        self.verify_connection = verify_connection
+
+        self.wallhaven_session = requests.Session()
+        self.request_lock = threading.Lock()
+
+        self.username = username
+        self.password = password
+
+        self.logged_in = False
+
+        if self.username is not None and len(self.username) and password is not None and len(self.password):
+            self._login()
+
+    def _login(self):
+        home_page = self._wallhaven_get("https://alpha.wallhaven.cc/", verify=self.verify_connection)
+
+        if home_page.status_code != 200:
+            return
+
+        token_tag = BeautifulSoup(home_page.text, "html.parser")\
+            .select('#login > input[name="_token"]')
+
+        if len(token_tag) == 0:
+            return
+
+        page = self._wallhaven_post("https://alpha.wallhaven.cc/auth/login", data={"username": self.username,
+                                                                                   "password": self.password,
+                                                                                   "_token":
+                                                                                       token_tag[0].attrs['value']},
+                                    verify=self.verify_connection)
+        if page.status_code != 200:
+            return
+
+        self.logged_in = True
+
+    def _wallhaven_post(self, url, data=None, json=None, **kwargs):
+        self.request_lock.acquire()
+        result = self.wallhaven_session.post(url, data, json, **kwargs)
+        self.request_lock.release()
+        return result
+
+    def _wallhaven_get(self, url, **kwargs):
+        self.request_lock.acquire()
+        result = self.wallhaven_session.get(url, **kwargs)
+        self.request_lock.release()
+        return result
 
     def get_images_urls(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
                         purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
@@ -19,31 +67,21 @@ class WallhavenApi:
 
         return self._make_images_urls(image_numbers)
 
-    @staticmethod
-    def _get_images_page_data(categories, purity, resolutions, ratios, sorting, order, page):
-        page_data = requests.get('https://alpha.wallhaven.cc/search', params={"categories": categories,
-                                                                              "purity": purity,
-                                                                              "resolutions": resolutions,
-                                                                              "ratios": ratios,
-                                                                              "sorting": sorting,
-                                                                              "order": order,
-                                                                              "page": page},
-                                 verify=False)
+    def _get_image_numbers(self, categories, purity, resolutions, ratios, sorting, order, page):
+        logging.debug("Trying obtain image number")
+
+        page_data = self._wallhaven_get('https://alpha.wallhaven.cc/search', params={"categories": categories,
+                                                                                     "purity": purity,
+                                                                                     "resolutions": resolutions,
+                                                                                     "ratios": ratios,
+                                                                                     "sorting": sorting,
+                                                                                     "order": order,
+                                                                                     "page": page},
+                                        verify=self.verify_connection)
 
         logging.debug("Page request code %d", page_data.status_code)
 
         if page_data.status_code != 200:
-            return None
-
-        return page_data
-
-    @staticmethod
-    def _get_image_numbers(categories, purity, resolutions, ratios, sorting, order, page):
-        logging.debug("Trying obtain image number")
-
-        page_data = WallhavenApi._get_images_page_data(categories, purity, resolutions, ratios, sorting, order, page)
-
-        if page_data is None:
             return None
 
         figures_tags = BeautifulSoup(page_data.text, "html.parser")\
@@ -63,10 +101,5 @@ class WallhavenApi:
 
     @staticmethod
     def _make_images_urls(images_numbers):
-        images_urls = []
-
-        for image_number in images_numbers:
-            images_urls.append('https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg'
-                               .format(str(image_number)))
-
-        return images_urls
+        return ['https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg' .format(str(image_number))
+                for image_number in images_numbers]

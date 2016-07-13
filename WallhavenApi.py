ad85805fe9e7a2e7978a53ce94a8e32f841a2cce
Goblenus <goblenus@ro.ru>
2016-07-13 21:43:47 +0300

    (Add) Added get_pages_count, get_images_numbers, is_image_exists, get_image_data methods

--- WallhavenApi.py
+++ WallhavenApi.py
@@ -53,23 +53,7 @@ class WallhavenApi:
         self.request_lock.release()
         return result
 
-    def get_images_urls(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
-                        purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
-                        page=1):
-        image_numbers = \
-            self._get_image_numbers(
-                str(int(category_general)) + str(int(category_anime)) + str(int(category_people)),
-                str(int(purity_sfw)) + str(int(purity_sketchy)) + str(int(purity_nsfw)),
-                resolutions, ratios, sorting, order, page)
-
-        if image_numbers is None:
-            return None
-
-        return self._make_images_urls(image_numbers)
-
-    def _get_image_numbers(self, categories, purity, resolutions, ratios, sorting, order, page):
-        logging.debug("Trying obtain image number")
-
+    def _get_images_page_data(self, categories, purity, resolutions, ratios, sorting, order, page):
         page_data = self._wallhaven_get('https://alpha.wallhaven.cc/search', params={"categories": categories,
                                                                                      "purity": purity,
                                                                                      "resolutions": resolutions,
@@ -80,6 +64,66 @@ class WallhavenApi:
                                         verify=self.verify_connection)
 
         logging.debug("Page request code %d", page_data.status_code)
+
+        return page_data
+
+    def _get_image_page_data(self, image_number):
+        logging.debug("Getting image page")
+        page_data = self._wallhaven_get('https://alpha.wallhaven.cc/wallpaper/{0}'.format(str(image_number)),
+                                        verify=self.verify_connection)
+
+        logging.debug("Page request code %d", page_data.status_code)
+
+        return page_data
+
+    @staticmethod
+    def _make_images_urls(images_numbers):
+        return ['https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg' .format(str(image_number))
+                for image_number in images_numbers]
+
+    @staticmethod
+    def make_image_url(image_number):
+        return 'https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg' .format(str(image_number))
+
+    def get_pages_count(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
+                        purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
+                        page=1):
+        page_data = self._get_images_page_data(
+            str(int(category_general)) + str(int(category_anime)) + str(int(category_people)),
+            str(int(purity_sfw)) + str(int(purity_sketchy)) + str(int(purity_nsfw)), resolutions, ratios, sorting,
+            order, page)
+
+        if page_data.status_code != 200:
+            return None
+
+        h2_tag = BeautifulSoup(page_data.text, "html.parser")\
+            .select("#thumbs > section:nth-of-type(1) > h2")
+
+        if not len(h2_tag):
+            return None
+
+        return int(h2_tag[0].text[h2_tag[0].text.rfind("/") + 1:])
+
+    def get_images_urls(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
+                        purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
+                        page=1):
+        image_numbers = self.get_images_numbers(category_general, category_anime, category_people, purity_sfw,
+                                                purity_sketchy, purity_nsfw, resolutions, ratios, sorting, order, page)
+
+        if image_numbers is None:
+            return None
+
+        return self._make_images_urls(image_numbers)
+
+    def get_images_numbers(self, category_general=True, category_anime=True, category_people=True, purity_sfw=True,
+                           purity_sketchy=True, purity_nsfw=False, resolutions="", ratios="", sorting="", order="desc",
+                           page=1):
+        logging.debug("Trying obtain image number")
+
+        page_data = self._get_images_page_data(
+            str(int(category_general)) + str(int(category_anime)) + str(int(category_people)),
+            str(int(purity_sfw)) + str(int(purity_sketchy)) + str(int(purity_nsfw)),
+            resolutions, ratios, sorting, order, page)
 
         if page_data.status_code != 200:
             return None
@@ -99,7 +143,152 @@ class WallhavenApi:
 
         return images_numbers
 
-    @staticmethod
-    def _make_images_urls(images_numbers):
-        return ['https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-{0}.jpg' .format(str(image_number))
-                for image_number in images_numbers]
+    def is_image_exists(self, image_number):
+        page_data = self._wallhaven_get("https://alpha.wallhaven.cc/wallpaper/{0}".format(str(image_number)))
+
+        if page_data.status_code != 200:
+            return False
+
+        section_tag = BeautifulSoup(page_data.text, "html.parser").select("#showcase")
+
+        if not len(section_tag):
+            return False
+
+        return True
+
+    def get_image_data(self, image_number):
+        resolution_tag_selector = "#showcase-sidebar > div > div.sidebar-content > h3"
+        color_tag_selector = "#showcase-sidebar > div > div.sidebar-content > ul > li"
+        tags_tags_selector = "#tags > li > a:nth-of-type(2)" if self.logged_in else "#tags > li > a:nth-of-type(1)"
+        sfw_tag_selector = "#wallpaper-purity-form label.purity.sfw"
+        sketchy_tag_selector = "#wallpaper-purity-form label.purity.sketchy"
+        nsfw_tag_selector = "#wallpaper-purity-form label.purity.nsfw"
+        avatar_tag_selector = "a.avatar.avatar-32 > img"
+        username_tag_selector = "a.username.usergroup-2"
+        upload_time_tag_selector = "dd.showcase-uploader > time"
+        category_tag_selector = "div[data-storage-id=showcase-info] > dl > dd:nth-of-type(2)"
+        size_tag_selector = "div[data-storage-id=showcase-info] > dl > dd:nth-of-type(3)"
+        views_tag_selector = "div[data-storage-id=showcase-info] > dl > dd:nth-of-type(4)"
+        favorites_tag_selector = "div[data-storage-id=showcase-info] > dl > dd:nth-of-type(5) > a"
+
+        page_data = self._get_image_page_data(image_number)
+
+        if page_data.status_code != 200:
+            return None
+
+        section_tag = BeautifulSoup(page_data.text, "html.parser").select("#showcase")
+        if not len(section_tag):
+            return None
+
+        image_data = {"ImageUrl": self.make_image_url(image_number),
+                      "Tags": [],
+                      "ImageColors": [],
+                      "Purity": None, # {"SFW": False, "Sketchy": False, "NSFW": False},
+                      "Uploader": {"Avatar": {"32": None, "200": None}, "Username": None},
+                      "ShortUrl": "https://whvn.cc/{0}".format(str(image_number)),
+                      "Resolution": None,
+                      "Ratio": None,
+                      "UploadTime": None,
+                      "Category": None,
+                      "Size": None,
+                      "Views": None,
+                      "Favorites": None}
+
+        resolution_tag = BeautifulSoup(page_data.text, "html.parser").select(resolution_tag_selector)
+        if len(resolution_tag):
+            resolution_tag = resolution_tag[0]
+            image_data["Resolution"] = str(resolution_tag.text).replace(" ", "")
+
+            image_data["Ratio"] = resolution_tag.attrs["title"]
+
+        color_tag = BeautifulSoup(page_data.text, "html.parser").select(color_tag_selector)
+        if len(color_tag):
+            for color_tag in color_tag:
+                style = color_tag.attrs["style"]
+                image_data["ImageColors"].append(style[style.rfind("#"):])
+
+        tags_tags = BeautifulSoup(page_data.text, "html.parser").select(tags_tags_selector)
+        if len(tags_tags):
+            for tag_tag in tags_tags:
+                image_data["Tags"].append(tag_tag.text)
+
+        sfw_tag = BeautifulSoup(page_data.text, "html.parser").select(sfw_tag_selector)
+        if len(sfw_tag):
+            if self.logged_in:
+                sfw_checkbox_tag = BeautifulSoup(page_data.text, "html.parser").select("#sfw")
+                if len(sfw_checkbox_tag):
+                    sfw_checkbox_tag = sfw_checkbox_tag[0]
+                    if sfw_checkbox_tag.has_attr('checked'):
+                        image_data["Purity"] = "SFW"
+                else:
+                    image_data["Purity"] = "SFW"
+            else:
+               image_data["Purity"] = "SFW"
+
+        if image_data["Purity"] is None:
+            sketchy_tag = BeautifulSoup(page_data.text, "html.parser").select(sketchy_tag_selector)
+            if len(sketchy_tag):
+                if self.logged_in:
+                    sketchy_checkbox_tag = BeautifulSoup(page_data.text, "html.parser").select("#sketchy")
+                    if len(sketchy_checkbox_tag):
+                        sketchy_checkbox_tag = sketchy_checkbox_tag[0]
+                        if sketchy_checkbox_tag.has_attr('checked'):
+                            image_data["Purity"] = "Sketchy"
+                    else:
+                        image_data["Purity"] = "Sketchy"
+                else:
+                   image_data["Purity"] = "Sketchy"
+
+        if image_data["Purity"] is None:
+            nsfw_tag = BeautifulSoup(page_data.text, "html.parser").select(nsfw_tag_selector)
+            if len(nsfw_tag):
+                if self.logged_in:
+                    nsfw_checkbox_tag = BeautifulSoup(page_data.text, "html.parser").select("#nsfw")
+                    if len(nsfw_checkbox_tag):
+                        nsfw_checkbox_tag = nsfw_checkbox_tag[0]
+                        if nsfw_checkbox_tag.has_attr('checked'):
+                            image_data["Purity"] = "NSFW"
+                    else:
+                        image_data["Purity"] = "NSFW"
+                else:
+                   image_data["Purity"] = "NSFW"
+
+        avatar_tag = BeautifulSoup(page_data.text, "html.parser").select(avatar_tag_selector)
+        if len(avatar_tag):
+            avatar_tag = avatar_tag[0]
+            avatar_url = avatar_tag.attrs["src"]
+            if avatar_url[:2] == "//":
+                avatar_url = avatar_url[2:]
+            image_data["Uploader"]["Avatar"] = {"32": avatar_url, "200": avatar_url.replace("/32/", "/200/")}
+
+        username_tag = BeautifulSoup(page_data.text, "html.parser").select(username_tag_selector)
+        if len(username_tag):
+            username_tag = username_tag[0]
+            image_data["Uploader"]["Username"] = username_tag.text
+
+        upload_time_tag = BeautifulSoup(page_data.text, "html.parser").select(upload_time_tag_selector)
+        if len(upload_time_tag):
+            upload_time_tag = upload_time_tag[0]
+            image_data["UploadTime"] = upload_time_tag.attrs["datetime"]
+
+        category_tag = BeautifulSoup(page_data.text, "html.parser").select(category_tag_selector)
+        if len(category_tag):
+            category_tag = category_tag[0]
+            image_data["Category"] = category_tag.text
+
+        size_tag = BeautifulSoup(page_data.text, "html.parser").select(size_tag_selector)
+        if len(size_tag):
+            size_tag = size_tag[0]
+            image_data["Size"] = size_tag.text
+
+        views_tag = BeautifulSoup(page_data.text, "html.parser").select(views_tag_selector)
+        if len(views_tag):
+            views_tag = views_tag[0]
+            image_data["Views"] = int(views_tag.text.replace(",", ""))
+
+        favorites_tag = BeautifulSoup(page_data.text, "html.parser").select(favorites_tag_selector)
+        if len(favorites_tag):
+            favorites_tag = favorites_tag[0]
+            image_data["Favorites"] = int(favorites_tag.text.replace(",", ""))
+
+        return image_data

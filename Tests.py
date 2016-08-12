b04c49a0e542c8c8f5aaebec4dc6b3896812a96a
Goblenus <goblenus@ro.ru>
2016-08-12 16:41:44 +0300

    (Add) Add some tests

--- Tests.py
+++ Tests.py
@@ -28,5 +28,47 @@ class TestLoggedUser(unittest.TestCase):
     def test_get_pages_count(self):
         self.assertIsNotNone(self.wa.get_pages_count())
 
+    def test_get_image_uploader(self):
+        uploader = self.wa.get_image_uploader("329194")
+        self.assertIsNotNone(uploader["Username"])
+        self.assertIsNotNone(uploader["Avatar"]["32"])
+        self.assertIsNotNone(uploader["Avatar"]["200"])
+
+    def test_get_image_category(self):
+        self.assertIsNotNone(self.wa.get_image_category("329194"))
+
+    def test_get_image_short_url(self):
+        self.assertIsNotNone(self.wa.get_image_short_url("329194"))
+
+    def test_get_image_upload_time(self):
+        self.assertIsNotNone(self.wa.get_image_upload_time("329194"))
+
+    def test_get_image_ratio(self):
+        self.assertIsNotNone(self.wa.get_image_ratio("329194"))
+
+    def test_get_image_resolution(self):
+        self.assertIsNotNone(self.wa.get_image_resolution("329194"))
+
+    def test_get_image_colors(self):
+        self.assertTrue(len(self.wa.get_image_colors("329194")) > 0)
+
+    def test_get_image_favorites(self):
+        self.assertIsNotNone(self.wa.get_image_favorites("329194"))
+
+    def test_get_image_url(self):
+        self.assertIsNotNone(self.wa.get_image_url("329194"))
+
+    def test_get_image_size(self):
+        self.assertIsNotNone(self.wa.get_image_size("329194"))
+
+    def test_get_image_purity(self):
+        self.assertIsNotNone(self.wa.get_image_purity("329194"))
+
+    def test_get_image_views(self):
+        self.assertIsNotNone(self.wa.get_image_views("329194"))
+
+    def test_get_image_tags_ex(self):
+        self.assertTrue(len(self.wa.get_image_tags_ex("329194")) > 0)
+
 if __name__ == '__main__':
     unittest.main()
